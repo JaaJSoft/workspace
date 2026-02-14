3ebc81e20d634832238773d5f9ffa40153a2be40
Pierre Chopinet <pierre.chopinet97@gmail.com>
2026-02-14 17:43:54 +0100

    fix(ui/auth): update login view to use new template path
    - Change `LoginView` to reference `users/auth/login.html` instead of the deleted `registration/login.html`.

--- workspace/urls.py
+++ workspace/urls.py
@@ -47,7 +47,7 @@ ui_urlpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Authentication
-    path('login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login', auth_views.LoginView.as_view(template_name='users/auth/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     # Health check
     path('health/', include('health_check.urls')),

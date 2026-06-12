3e6f19fff24767922b69d10406f17c285119d0ce
Pierre Chopinet <pierre.chopinet97@gmail.com>
2026-06-12 12:42:44 +0200

    feat(core): exclude files module from access restriction

--- workspace/files/apps.py
+++ workspace/files/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class FilesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "workspace.files"
-    restrictable = True
 
     def ready(self):
         from workspace.core.module_registry import (

c4a44626f7b71642e47b3e5cffd3d7c5a028425f
pchopinet <pierre.chopinet@jaaj.dev>
2026-08-23 19:38:30 +0200

    style(projects): apply ruff formatting

--- workspace/projects/tests/test_ui_sprints.py
+++ workspace/projects/tests/test_ui_sprints.py
@@ -95,13 +95,9 @@ class ScrumBoardUiTests(ScrumUiTestCase):
     def test_switcher_closed_toggle_only_with_history(self):
         self.scrum.sprints.create(name="Running", state=Sprint.State.ACTIVE)
         self.client.force_login(self.member)
-        self.assertNotContains(
-            self.client.get(self.board_url), "Hide closed sprints"
-        )
+        self.assertNotContains(self.client.get(self.board_url), "Hide closed sprints")
         self.scrum.sprints.create(name="Done one", state=Sprint.State.CLOSED)
-        self.assertContains(
-            self.client.get(self.board_url), "Hide closed sprints"
-        )
+        self.assertContains(self.client.get(self.board_url), "Hide closed sprints")
 
     def test_switcher_search_appears_past_five_sprints(self):
         self.scrum.sprints.create(name="Running", state=Sprint.State.ACTIVE)

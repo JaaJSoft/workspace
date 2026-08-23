231905c3ad4681da43409ddcabec44fbc6beb214
pchopinet <pierre.chopinet@jaaj.dev>
2026-08-23 18:54:45 +0200

    chore(projects): renumber sprint migration after task watchers

--- workspace/projects/migrations/0026_alter_project_type_alter_taskevent_type_sprint_and_more.py
+++ workspace/projects/migrations/0026_alter_project_type_alter_taskevent_type_sprint_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0024_projectnotificationlevel'),
+        ('projects', '0025_taskwatcher'),
     ]
 
     operations = [

96ff029d838378468ff3ea87a4b3eea092951552
Pierre Chopinet <pierre.chopinet97@gmail.com>
2026-09-05 16:13:06 +0200

    style(files): drop a duplicated import in the public links tests

--- workspace/files/tests/test_public_links.py
+++ workspace/files/tests/test_public_links.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-from django.contrib.auth.models import Group
 from django.test import TestCase
 
 from workspace.files.models import File, FileShareLink
